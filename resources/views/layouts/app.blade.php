b5f8555b25e5a5e13dafa60de7c3a17887849d45
相良征哉 <sagarayuya@sagarasusumunarinoMacBook-Pro.local>
2023-11-12 18:19:57 +0900

    社名の変更 refs feature/fix_top_page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" rel="stylesheet">
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-    <title>Attractive Japan Tours</title>
+    <title>EATravel Japan</title>
 </head>
 <body>
     <header id="header" class="d-flex flex-column justify-content-between">
